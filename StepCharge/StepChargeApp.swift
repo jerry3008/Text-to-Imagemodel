5d51ff617735e44b9a125f182054d3419d6718a1
ChefAharoni <ChefAharoni@proton.me>
2023-12-06 10:32:16 -0500

    Initial Commit

---
+++ StepCharge/StepChargeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepChargeApp.swift
+//  StepCharge
+//
+//  Created by Amit Aharoni on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct StepChargeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
